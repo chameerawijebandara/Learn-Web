7a44ed41e5c024c27a7f7af65b2300fff86b6e6e
chameerawijebandara <chameerawijebandara@gmail.com>
2015-05-28 23:57:02 +0530

    Refact the code

--- Simple_Alarm/js/index.js
+++ Simple_Alarm/js/index.js
@@ -4,108 +4,113 @@ var maxSecs;
 var mins;
 var secs;
 var soundTrack;
-	
+
 function btnStartOnClick(btnStart) {
-	
-	console.log("Start btn clicked");
-	if (btnStart.innerHTML.toUpperCase() == "START")
-	{		
-		btnStart.innerHTML = "Pause";
-		timer = setInterval(timeOut, 1000);
-	} 
-   else
+
+    console.log("Start btn clicked");
+    if (btnStart.innerHTML.toUpperCase() === "START")
     {
-		btnStart.innerHTML = "Start";	
-		clearInterval(timer);
-	}
-};
+        btnStart.innerHTML = "Pause";
+        timer = setInterval(timeOut, 1000);
+    }
+    else
+    {
+        btnStart.innerHTML = "Start";
+        clearInterval(timer);
+    }
+}
+;
 
 function btnStopOnClick(btnStop) {
-	console.log("Stop btn clicked");
-	
-	clearInterval(timer);
-	$("#btn_start").innerHTML = "Start";	
-	mins = maxMins;
-	secs = maxSecs;
-	displayTime();
-};
+    console.log("Stop btn clicked");
+
+    clearInterval(timer);
+    $("#btn_start").innerHTML = "Start";
+    mins = maxMins;
+    secs = maxSecs;
+    displayTime();
+}
+;
 
 function btnSettingsClick() {
-	console.log("Setting btn clicked");
-	window.location.href = "settings.html";
+    console.log("Setting btn clicked");
+    window.location.href = "settings.html";
 }
 function timeOut() {
-	console.log("Timer tics");
-	secs--;
-	if(secs < 0)
-	{
-		mins--;
-		
-		secs = 59; 
-	}
-	if( secs<0 || mins < 0)
-	{
-		document.getElementById("btn_start").innerHTML = "Start";	
-		return;
-	}
-	displayTime();
-	
-	if(mins + secs == 0)
-	{
-		document.getElementById("btn_start").innerHTML = "Start";	
-		var audio = new Audio(soundTrack);
-		audio.play();
-		document.getElementById("clock").innerHTML = "Time Passed";
-		clearInterval(timer);
-	}	
-};
+    console.log("Timer tics");
+    secs--;
+    if (secs < 0)
+    {
+        mins--;
+
+        secs = 59;
+    }
+    if (secs < 0 || mins < 0)
+    {
+        document.getElementById("btn_start").innerHTML = "Start";
+        return;
+    }
+    displayTime();
+
+    if (mins + secs === 0)
+    {
+        document.getElementById("btn_start").innerHTML = "Start";
+        var audio = new Audio(soundTrack);
+        audio.play();
+        document.getElementById("clock").innerHTML = "Time Passed";
+        clearInterval(timer);
+    }
+}
+;
 
 function formatTime() {
-	if(mins < 10)
-	{
-		if(secs < 10)
-		{
-			return "0" + mins + " : " + "0" + secs;
-		}
-		else
-		{
-			return "0" + mins + " : " + secs;
-		}
-	}
-	else
-	{
-		if(secs < 10)
-		{
-			return mins + " : " + "0" + secs;
-		}
-		else
-		{
-			return mins + " : " + secs;
-		}
-	}
-	
-};
+    if (mins < 10)
+    {
+        if (secs < 10)
+        {
+            return "0" + mins + " : " + "0" + secs;
+        }
+        else
+        {
+            return "0" + mins + " : " + secs;
+        }
+    }
+    else
+    {
+        if (secs < 10)
+        {
+            return mins + " : " + "0" + secs;
+        }
+        else
+        {
+            return mins + " : " + secs;
+        }
+    }
+
+}
+;
 
 function displayTime() {
-	if(document.getElementById("clock"))
-	{		
-		document.getElementById("clock").innerHTML = formatTime();
-	}
-};
+    if (document.getElementById("clock"))
+    {
+        document.getElementById("clock").innerHTML = formatTime();
+    }
+}
+;
 
 
-window.onload = function(){
-	console.log("Page loaded");
-	
-	maxMins = LocalStorage.getMaxMins();
-	maxSecs = LocalStorage.getMaxSecs();
-	
-	mins = maxMins;
-	secs = maxSecs;
+window.onload = function() {
+    console.log("Page loaded");
 
-	document.bgColor = LocalStorage.getBackColor();
-	soundTrack = LocalStorage.getSoundTrack();
-		
-	displayTime();
+    maxMins = LocalStorage.getMaxMins();
+    maxSecs = LocalStorage.getMaxSecs();
+
+    mins = maxMins;
+    secs = maxSecs;
+
+    document.bgColor = LocalStorage.getBackColor();
+    soundTrack = LocalStorage.getSoundTrack();
+
+    displayTime();
 };
 	 
--- Simple_Alarm/js/settings.js
+++ Simple_Alarm/js/settings.js
@@ -1,44 +1,44 @@
 function btnSubmitOnClick(params) {
-	console.log("Submit btn clicked");
+    console.log("Submit btn clicked");
 
-	LocalStorage.setMaxMins(document.getElementById("set_mins").value);
-	LocalStorage.setMaxSecs(document.getElementById("set_secs").value);
-	LocalStorage.setBackColor(document.getElementById("set_color").value);
-	
-	if(document.getElementById("set_color").value)
-	{
-		LocalStorage.setSoundTrack(document.getElementById("set_sound").value);	
-	}
-	window.location.href = "index.html";
+    DB.setMaxMins(document.getElementById("set_mins").value);
+    DB.setMaxSecs(document.getElementById("set_secs").value);
+    DB.setBackColor(document.getElementById("set_color").value);
+
+    if (document.getElementById("set_color").value)
+    {
+        DB.setSoundTrack(document.getElementById("set_sound").value);
+    }
+    window.location.href = "index.html";
 }
 
 function btnCancelOnClick(params) {
-	console.log("Cancel btn clicked");
-	
-	var r = confirm("All your changes will be discarded.");
-	if (r != true) {
-	  return;
-	}
-	window.location.href = "index.html";
+    console.log("Cancel btn clicked");
+
+    var r = confirm("All your changes will be discarded.");
+    if (r !== true) {
+        return;
+    }
+    window.location.href = "index.html";
 }
 
 function btnDefaultOnClick(params) {
-	console.log("Default btn clicked");
-	
-	var r = confirm("All your changes will be discarded.");
-	if (r != true) {
-	  return;
-	}
-	
-	LocalStorage.free();	
-	window.location.href = "index.html";
+    console.log("Default btn clicked");
+
+    var r = confirm("All your changes will be discarded.");
+    if (r !== true) {
+        return;
+    }
+
+    DB.free();
+    window.location.href = "index.html";
 }
-window.onload = function(){
-	console.log("Page loaded");
-	
-	document.getElementById("set_mins").value = LocalStorage.getMaxMins();
-	document.getElementById("set_secs").value = LocalStorage.getMaxSecs();
-	document.getElementById("set_color").value = LocalStorage.getBackColor();
-	
+window.onload = function() {
+    console.log("Page loaded");
+
+    document.getElementById("set_mins").value = DB.getMaxMins();
+    document.getElementById("set_secs").value = DB.getMaxSecs();
+    document.getElementById("set_color").value = DB.getBackColor();
+
 };
 	 
